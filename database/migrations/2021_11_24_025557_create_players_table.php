a2a0c86488d3f90a1c615541d6090f25eaf54459
hyseanlin <hylin827@gmail.com>
2021-11-24 15:30:00 +0800

    利用migration建立空的表格

---
+++ database/migrations/2021_11_24_025557_create_players_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id()->comment('編號');
+            $table->string('name')->comment('姓名');
+            $table->foreignId('tid')->comment('球隊編號');
+            $table->date('birthdate')->default('1916-01-01')->nullable()->comment('生日');;
+            $table->date('onboarddate')->default('1946-06-06')->nullable()->comment('到職日');;
+            $table->string('position')->comment('位置');
+            $table->float('height')->comment('身高');
+            $table->float('weight')->comment('體重');
+            $table->tinyInteger('year')->unsigned()->comment('年資');
+            $table->string('nationality')->comment('國籍');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
